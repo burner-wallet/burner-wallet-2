a57f801306b229760e4dcbfaf998d913ae7ff9e5
David Mihal <david@dmihal.com>
2019-06-03 11:01:26 +0300

    Display account in header

--- burner-ui/src/components/Header.tsx
+++ burner-ui/src/components/Header.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Header.css';
+import { withBurner, BurnerContext } from '../BurnerProvider';
 
-const Header: React.FC = () => (
-  <header>Burner Wallet</header>
+const Header: React.FC<BurnerContext> = ({ accounts }) => (
+  <header>
+    <div className="headerAccount">{accounts.length > 0 && accounts[0].substr(2, 8)}</div>
+    Burner Wallet
+  </header>
 );
 
-export default Header;
+export default withBurner(Header);
