6ea4f86942a7d5538ce37abd68caed0e6e7dfe2b
David Mihal <david@dmihal.com>
2019-06-08 18:09:44 +0300

    Display form on exchange page

--- exchange/src/ui/PairUI.tsx
+++ exchange/src/ui/PairUI.tsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './PairUI.module.css';
 
 export default class PairUI extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      balanceA: '0',
-      balanceB: '0',
+      mode: 'default',
+      amount: '0',
     }
     this.assetA = this.getAsset(props.pair.assetA);
     this.assetB = this.getAsset(props.pair.assetB);
@@ -21,12 +21,43 @@ export default class PairUI extends Component {
     throw new Error(`Could not find asset ${id}`);
   }
 
-  render() {
+  buttons() {
     const { pair } = this.props
     return (
+      <Fragment>
+        <button className={classes.exchangeBtn} onClick={() => this.setState({ mode: 'AtoB' })}>
+          {this.assetA.name} to {this.assetB.name}
+        </button>
+        <button className={classes.exchangeBtn} onClick={() => this.setState({ mode: 'BtoA' })}>
+          {this.assetB.name} to {this.assetA.name}
+        </button>
+      </Fragment>
+    );
+  }
+
+  form() {
+    const { mode, amount } = this.state;
+    const { assetA, assetB } = this;
+    return (
+      <Fragment>
+        <div>
+          {mode === 'AtoB' ? `${assetA.name} to ${assetB.name}` : `${assetB.name} to ${assetA.name}`}
+        </div>
+        <input type="number" value={amount} onChange={e => this.setState({ amount: e.target.value })} />
+        <button type="button" onClick={() => this.setState({ mode: 'default', amount: '0' })}>
+          Cancel
+        </button>
+        <button type="button" onClick={() => this.setState({ mode: 'default', amount: '0' })}>
+          Exchange
+        </button>
+      </Fragment>
+    );
+  }
+
+  render() {
+    return (
       <div className={classes.pair}>
-        <button className={classes.exchangeBtn}>{this.assetA.name} to {this.assetB.name}</button>
-        <button className={classes.exchangeBtn}>{this.assetB.name} to {this.assetA.name}</button>
+        {this.state.mode === 'default' ? this.buttons() : this.form()}
       </div>
     );
   }
