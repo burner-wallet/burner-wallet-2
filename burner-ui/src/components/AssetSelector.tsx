3f8a944204a746c1d17fbd3ff943cd3cb51564a2
David Mihal <david@dmihal.com>
2019-08-06 08:50:16 +0200

    Improve AssetSelector

--- burner-ui/src/components/AssetSelector.tsx
+++ burner-ui/src/components/AssetSelector.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Asset } from '@burner-wallet/assets';
 import AccountBalance, { AccountBalanceData } from '../data-providers/AccountBalance';
 import Assets from '../data-providers/Assets';
@@ -17,40 +17,48 @@ const AssetElement: React.FC<{asset: Asset}> = ({ asset }) => (
 
 export interface AssetSelectorProps {
   selected: Asset | null;
+  assets?: Asset[],
   onChange: (asset: Asset) => void;
   network?: string;
   disabled?: boolean;
 }
 
-const AssetSelector: React.FC<AssetSelectorProps> = ({ selected, onChange, disabled, network }) => {
+const AssetSelector: React.FC<AssetSelectorProps> = ({ selected, assets, onChange, disabled, network }) => {
   const [isOpen, setOpen] = useState(false);
-  return (
-    <Assets render={(assets: Asset[]) => {
-      if (!selected) {
-        onChange(assets[0]);
-      }
-      const selectedAsset = assets.filter(asset => asset === selected)[0];
-      return (
-        <div className={classes.selectContainer}>
-          <div onClick={() => setOpen(!isOpen)} className={classes.selectBox}>
-            {selectedAsset && <AssetElement asset={selectedAsset} />}
-          </div>
-          {isOpen && !disabled && (
-            <div className={classes.selectDropdown}>
-              {assets.filter(asset => !network || asset.network === network).map(asset => (
-                <div key={asset.id} className={classes.dropdownItem} onClick={() => {
-                  setOpen(false);
-                  onChange(asset);
-                }}>
-                  <AssetElement asset={asset} />
-                </div>
-              ))}
-            </div>
-          )}
+
+  useEffect(() => {
+    const clickListener = () => setOpen(false);
+    document.addEventListener('click', clickListener);
+    return () => document.removeEventListener('click', clickListener);
+  });
+
+  const Dropdown = (assets: Asset[]) => {
+    if (!selected) {
+      onChange(assets[0]);
+    }
+    const selectedAsset = assets.filter(asset => asset === selected)[0];
+    return (
+      <div className={classes.selectContainer}>
+        <div onClick={() => setOpen(!isOpen)} className={classes.selectBox}>
+          {selectedAsset && <AssetElement asset={selectedAsset} />}
         </div>
-      );
-    }} />
-  );
+        {isOpen && !disabled && (
+          <div className={classes.selectDropdown}>
+            {assets.filter(asset => !network || asset.network === network).map(asset => (
+              <div key={asset.id} className={classes.dropdownItem} onClick={() => {
+                setOpen(false);
+                onChange(asset);
+              }}>
+                <AssetElement asset={asset} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return assets ? Dropdown(assets) : <Assets render={Dropdown} />;
 };
 
 export default AssetSelector;
