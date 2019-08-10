76b1e8d8ee4bdbb3d0b665082665b73baab0b1e0
David Mihal <david@dmihal.com>
2019-08-10 01:43:59 -0400

    Reformat header

--- burner-ui/src/components/Header.tsx
+++ burner-ui/src/components/Header.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import './Header.css';
 import { withBurner, BurnerContext } from '../BurnerProvider';
+const classes = require('./Header.module.css');
 
 interface HeaderProps {
   title?: string,
 }
 
 const Header: React.FC<BurnerContext & HeaderProps> = ({ accounts, title }) => (
-  <header>
-    <div className="headerAccount">{accounts.length > 0 && accounts[0].substr(2, 8)}</div>
-    {title}
+  <header className={classes.header}>
+    <div className={classes.titleContainer}>
+      <div className={classes.title}>{title}</div>
+      {title !== 'Burner Wallet' && (
+        <div className={classes.subtitle}>Powered by Burner Wallet</div>
+      )}
+    </div>
+    <div className={classes.spacer}/>
+    <div className={classes.headerAccount}>{accounts.length > 0 && accounts[0].substr(2, 8)}</div>
   </header>
 );
 
