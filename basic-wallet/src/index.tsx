2da429ca0e3544f71e6ee3f1782b70d3d530eb1b
David Mihal <david@dmihal.com>
2019-08-23 23:19:06 +0200

    Add basic-wallet

---
+++ basic-wallet/src/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import { xdai, dai, eth } from '@burner-wallet/assets';
+import BurnerCore from '@burner-wallet/core';
+import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers';
+import { InfuraGateway, InjectedGateway, XDaiGateway, } from '@burner-wallet/core/gateways';
+import Exchange from '@burner-wallet/exchange';
+import { xdaiBridge, uniswapDai } from '@burner-wallet/exchange/pairs';
+import BurnerUI from '@burner-wallet/ui';
+import LegacyPlugin from '@burner-wallet/plugins/legacy';
+
+const core = new BurnerCore({
+  signers: [new InjectedSigner(), new LocalSigner()],
+  gateways: [
+    new InjectedGateway(),
+    new InfuraGateway(process.env.REACT_APP_INFURA_KEY),
+    new XDaiGateway(),
+  ],
+});
+
+const exchange = new Exchange({
+  pairs: [xdaiBridge, uniswapDai],
+});
+
+const BurnerWallet = () =>
+  <BurnerUI
+    core={core}
+    assets={[xdai, dai, eth]}
+    plugins={[exchange, new LegacyPlugin()]}
+  />
+
+
+
+ReactDOM.render(<BurnerWallet />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
