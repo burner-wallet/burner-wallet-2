1df5866595be3e207ab1429113d29f0d5a7377c1
David Mihal <david@dmihal.com>
2019-08-24 00:26:23 +0200

    Copy address on click

--- burner-ui/src/Pages/ReceivePage/ReceivePage.tsx
+++ burner-ui/src/Pages/ReceivePage/ReceivePage.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QRCode from 'qrcode.react';
 import { BurnerContext, withBurner } from '../../BurnerProvider';
 import Page from '../../components/Page';
 const classes = require('./ReceivePage.module.css');
 
-const ReceivePage: React.FC<BurnerContext> = ({ accounts }) => (
-  <Page title="Receive">
-    {accounts.length > 0 && (
-      <>
-        <div className={classes.qrContainer}>
-          <QRCode value={accounts[0]} renderAs="svg" />
-        </div>
-        <div className={classes.addressContainer}>
-          <input value={accounts[0]} readOnly className={classes.address} />
-        </div>
-      </>
-    )}
-  </Page>
-);
+const ReceivePage: React.FC<BurnerContext> = ({ accounts }) => {
+  const [showCopied, setShowCopied] = useState(false);
+  return (
+    <Page title="Receive">
+      {accounts.length > 0 && (
+        <>
+          <div className={classes.qrContainer}>
+            <QRCode value={accounts[0]} renderAs="svg" />
+          </div>
+          <div className={classes.addressContainer}>
+            <input
+              value={accounts[0]}
+              readOnly
+              className={classes.address}
+              onClick={() => {
+                navigator.clipboard.writeText(accounts[0]);
+                setShowCopied(true);
+                setTimeout(() => setShowCopied(false), 5000);
+              }}
+            />
+          </div>
+          <div className={showCopied ? classes.copied : classes.copyHidden}>
+            Address copied to clipboard
+          </div>
+        </>
+      )}
+    </Page>
+  );
+};
 
 export default withBurner(ReceivePage);
