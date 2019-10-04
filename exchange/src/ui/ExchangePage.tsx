aa898949c4b09f1205b38380e7c3d160229f6661
David Mihal <david@dmihal.com>
2019-10-04 13:38:36 +0100

    Update exchange to newer UI

--- exchange/src/ui/ExchangePage.tsx
+++ exchange/src/ui/ExchangePage.tsx
@@ -99,7 +99,7 @@ export default class ExchangePage extends Component<PluginPageContext, ExchangeP
           <AmountInput
             asset={assetA}
             value={amount}
-            onChange={e => this.setState({ amount: e.target.value })}
+            onChange={newVal => this.setState({ amount: newVal })}
             disabled={isExchanging}
           />
         </div>
