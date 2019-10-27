b25586cd111b88662461756641ae599622f0fff6
David Mihal <david@dmihal.com>
2019-10-27 12:38:43 -0500

    Set up ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  extends: [
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    }
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off", // TODO: Reduce the use of 'any'
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "react/prop-types": "off",
+  },
+  settings: {
+    react: {
+      version: "16.8.6",
+    }
+  }
+};
