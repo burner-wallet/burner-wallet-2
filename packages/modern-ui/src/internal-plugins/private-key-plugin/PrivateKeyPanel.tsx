ee0b72d7151b9e5a6eb1242a34409a7857e6873c
David Mihal <david@dmihal.com>
2020-02-02 18:37:52 -0500

    Support burning contract wallets

--- packages/modern-ui/src/internal-plugins/private-key-plugin/PrivateKeyPanel.tsx
+++ packages/modern-ui/src/internal-plugins/private-key-plugin/PrivateKeyPanel.tsx
@@ -20,21 +20,33 @@ const PrivateKeyPanel: React.FC<PluginElementContext> = ({ actions, defaultAccou
       <h2>Private Key</h2>
       <AccountKeys
         account={defaultAccount}
-        render={(keys: Keys | null) =>
-          keys ? (
-            <Fragment>
-              <PrivateKeyField privateKey={keys.privateKey} />
+        render={(keys: Keys | null) => {
+          if (keys) {
+            return (
+              <Fragment>
+                <PrivateKeyField privateKey={keys.privateKey} />
 
-              <ImportPK onImport={(newPk: string) => actions.safeSetPK(newPk)} />
+                <ImportPK onImport={(newPk: string) => actions.safeSetPK(newPk)} />
 
+                <AdvancedButton onClick={() => actions.safeSetPK(randomHex(64))}>
+                  {t('Burn Account')}
+                </AdvancedButton>
+              </Fragment>
+            );
+          }
+
+          if (actions.canCallSigner('writeKey', defaultAccount)) {
+            return (
               <AdvancedButton onClick={() => actions.safeSetPK(randomHex(64))}>
                 {t('Burn Account')}
               </AdvancedButton>
-            </Fragment>
-          ) : (
+            );
+          }
+
+          return (
             <div>Private key unavailable while using a web3 browser such as Metamask</div>
-          )
-        }
+          );
+        }}
       />
     </section>
   );
