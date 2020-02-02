dde9239fc163c61ae9f868fc3bc45d06f37ec523
David Mihal <david@dmihal.com>
2020-02-01 19:09:00 -0500

    ENS 1.0.3 - updated registry

--- packages/ens-plugin/src/ENS.ts
+++ packages/ens-plugin/src/ENS.ts
@@ -4,7 +4,7 @@ import { registryInterface, resolverInterface } from './abi';
 const ZERO = '0x0000000000000000000000000000000000000000';
 
 const registrarAddress: { [network: string]: string } = {
-  '1': '0x314159265dd8dbb310642f98f50c066173c1259b',
+  '1': '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
   '3': '0x112234455c3a32fd11230c42e7bccd4a84e02010',
   '4': '0xe7410170f87102df0055eb195163a03b7f2bff4a',
   '5': '0x112234455c3a32fd11230c42e7bccd4a84e02010',
