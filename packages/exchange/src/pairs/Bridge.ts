7c0672faaaf248f18068717766a04bb79f33962d
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-04-08 11:04:34 -0300

    Wait for asset relay in bridge exchange operation

---
+++ packages/exchange/src/pairs/Bridge.ts
@@ -0,0 +1,87 @@
+import Pair, { ExchangeParams, ValueTypes } from './Pair';
+import { Asset } from '@burner-wallet/types';
+import promiseRetry from 'promise-retry';
+import Web3 from "web3";
+const { toBN } = Web3.utils;
+
+
+interface BridgePairConstructor {
+    assetA: string;
+    assetABridge: string;
+    assetB: string;
+    assetBBridge: string;
+}
+
+interface BridgeExchangeParams {
+    asset: Asset;
+    assetOnOtherNetwork: Asset;
+    from: string;
+    value: string;
+    to: string;
+}
+
+export default class Bridge extends Pair {
+    private readonly assetABridge: string;
+    private readonly assetBBridge: string;
+
+    constructor({ assetA, assetABridge, assetB, assetBBridge }: BridgePairConstructor) {
+        super({ assetA, assetB });
+        this.assetABridge = assetABridge;
+        this.assetBBridge = assetBBridge;
+    }
+
+    exchangeAtoB({ account, value, ether }: ExchangeParams) {
+        const _value = this._getValue({ value, ether });
+        const asset = this.getExchange().getAsset(this.assetA);
+        const assetOnOtherNetwork = this.getExchange().getAsset(this.assetB);
+        return this._exchangeAndWait({
+            asset,
+            assetOnOtherNetwork,
+            from: account,
+            value: _value,
+            to: this.assetABridge
+        });
+    }
+
+    exchangeBtoA({ account, value, ether }: ExchangeParams) {
+        const _value = this._getValue({ value, ether });
+        const asset = this.getExchange().getAsset(this.assetB);
+        const assetOnOtherNetwork = this.getExchange().getAsset(this.assetA);
+        return this._exchangeAndWait({
+            asset,
+            assetOnOtherNetwork,
+            from: account,
+            value: _value,
+            to: this.assetBBridge
+        });
+    }
+
+    async _exchangeAndWait({ asset, assetOnOtherNetwork, from, value, to }: BridgeExchangeParams) {
+        const initialBalance = await assetOnOtherNetwork.getBalance(from);
+        const result = await asset.send({ from, value, to });
+        await promiseRetry(async (retry) => {
+            const updatedBalance = await assetOnOtherNetwork.getBalance(from);
+            console.log("updated balance", updatedBalance.toString())
+            if (!toBN(updatedBalance).gt(toBN(initialBalance))) {
+                retry(null);
+            }
+        }, {
+            forever: true,
+            factor: 1.2
+        });
+
+        return result;
+    }
+
+    async estimateAtoB(value: ValueTypes) {
+        return this._getValue(value);
+    }
+
+    async estimateBtoA(value: ValueTypes) {
+        return this._getValue(value);
+    }
+
+    getLoadingMessage(): string {
+        return 'Exchanging assets.. please wait until the bridge relays the transaction';
+    }
+}
--- packages/exchange/src/pairs/XDaiBridge.ts
+++ packages/exchange/src/pairs/XDaiBridge.ts
@@ -1,39 +1,12 @@
-import Pair, { ExchangeParams, ValueTypes } from './Pair';
+import Bridge from "./Bridge";
 
-const toXdaiBridge = '0x4aa42145Aa6Ebf72e164C9bBC74fbD3788045016';
-const toDaiBridge = '0x7301cfa0e1756b71869e93d4e4dca5c7d0eb0aa6';
-
-export default class XDaiBridge extends Pair {
+export default class XDaiBridge extends Bridge {
   constructor() {
-    super({ assetA: 'xdai', assetB: 'dai' });
-  }
-
-  exchangeAtoB({ account, value, ether }: ExchangeParams) {
-    const _value = this._getValue({ value, ether });
-    const xdai = this.getExchange().getAsset('xdai');
-    return xdai.send({
-      from: account,
-      value: _value,
-      to: toDaiBridge,
+    super({
+      assetA: 'xdai',
+      assetABridge: '0x7301cfa0e1756b71869e93d4e4dca5c7d0eb0aa6',
+      assetB: 'dai',
+      assetBBridge: '0x4aa42145Aa6Ebf72e164C9bBC74fbD3788045016'
     });
-  }
-
-  exchangeBtoA({ account, value, ether }: ExchangeParams) {
-    const _value = this._getValue({ value, ether });
-
-    const dai = this.getExchange().getAsset(this.assetB);
-    return dai.send({
-      from: account,
-      value: _value,
-      to: toXdaiBridge,
-    });
-  }
-
-  async estimateAtoB(value: ValueTypes) {
-    return this._getValue(value);
-  }
-
-  async estimateBtoA(value: ValueTypes) {
-    return this._getValue(value);
   }
 }
